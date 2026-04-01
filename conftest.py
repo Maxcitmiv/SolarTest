58dd0abe6afc74488cb267d489db55dc5b507767
Maxcitmiv <maksmaks5213@gmail.com>
2026-04-01 12:11:52 +0300

    подготовка проекта для запуска на jenkins+selenoid

--- conftest.py
+++ conftest.py
@@ -1,11 +1,57 @@
 import pytest
 from selene import browser
+from selenium.webdriver.remote.client_config import ClientConfig
 
+from utils.attach import attach_video, attach_screenshot,attach_console_log,attach_page_source
+from selenium import webdriver
+from config.settings import *
+
+SELENOID_URL='https://selenoid.autotests.cloud/wd/hub'
+VIDEO_BASE_URL='https://selenoid.autotests.cloud/video'
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report=outcome.get_result()
+    setattr(item,f'rep_{report.when}',report)
 
 @pytest.fixture(autouse=True)
-def browser_config():
+def browser_function(request):
+    test_name=request.node.name
+    video_name=f'{test_name}.mp4'
+    options = webdriver.ChromeOptions()
+
+    options.set_capability("browserName", "chrome")
+    options.set_capability("goog:loggingPrefs", {"browser": "ALL"})
+    options.set_capability("selenoid:options", {
+        "enableVideo": True,
+        "enableVNC": True,
+        "name": test_name,
+        "videoName": video_name,
+    })
+
+    driver = webdriver.Remote(
+        command_executor=SELENOID_URL,
+        options=options,
+        client_config=ClientConfig(
+            remote_server_addr=SELENOID_URL,
+            username=LOGIN,
+            password=PASSWORD,
+        ),
+    )
+    browser.config.driver=driver
     browser.config.base_url = 'https://rt-solar.ru'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
+
     yield
-    browser.quit()
+
+
+    attach_screenshot()
+    attach_console_log()
+    attach_page_source()
+
+    driver.quit()
+
+    video_url=f'{VIDEO_BASE_URL}/{video_name}'
+    attach_video(video_url)
---
+++ utils/attach.py
@@ -0,0 +1,25 @@
+import json
+from pathlib import Path
+import allure
+from selene import browser
+
+
+def attach_screenshot():
+    png=browser.driver.get_screenshot_as_png()
+    allure.attach(png,name='screenshot',attachment_type=allure.attachment_type.PNG)
+
+def attach_page_source():
+    html=browser.driver.page_source
+    allure.attach(html,name='page_source',attachment_type=allure.attachment_type.HTML)
+
+def attach_console_log():
+    try:
+        logs=browser.driver.get_log('browser')
+        formatted_logs=json.dumps(logs,indent=2,ensure_ascii=False)
+    except Exception as e:
+        formatted_logs=f'Не удалось получить console logs: {e}'
+
+    allure.attach(formatted_logs,name='console_log',attachment_type=allure.attachment_type.JSON)
+
+def attach_video(video_url:str):
+    allure.attach(video_url,name='video_url',attachment_type=allure.attachment_type.URI_LIST)
